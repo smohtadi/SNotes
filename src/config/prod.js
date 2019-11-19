b51d6b04ae172a222f50424f2779a165c33c086d
smohtadi <salmanmohtadi@gmail.com>
2019-11-18 18:53:14 -0800

    fix env vars

--- src/config/prod.js
+++ src/config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-  MONGO_URI = process.env.MONGO_URI,
+  MONGO_URI: process.env.MONGO_URI,
   SESS_NAME: process.env.SESS_NAME,
   SESS_SECRET: process.env.SESS_SECRET,
   SESS_LIFETIME: process.env.SESS_LIFETIME,
